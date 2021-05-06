99a288b1e3af3bfb0440d573e8e8d812adb03468
Zach Sherman <zach@ngrok.com>
2021-05-06 09:10:27 -0700

    fix version strings in formulae

--- Formula/ngrok.rb
+++ Formula/ngrok.rb
@@ -3,7 +3,7 @@ class Ngrok < Formula
   homepage 'https://ngrok.com'
   url 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-arm64.zip'
   sha256 '1740437cbe26bf2766b059652b829b9e5c5116c0251d486189d338f0e280c256'
-  version :latest
+  version '2.3.40'
 
   def install
     bin.install 'ngrok'
