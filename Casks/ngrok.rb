10e97f7e836661e294a035fcc9d5c4eeb6ee67d6
Zach Sherman <zach@ngrok.com>
2021-05-06 09:36:19 -0700

    fix sha256 for cask

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -3,7 +3,7 @@ cask "ngrok" do
 
   if Hardware::CPU.intel?
     url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip"
-    sha256 "1740437cbe26bf2766b059652b829b9e5c5116c0251d486189d338f0e280c256"
+    sha256 "3e40376cfff0f7c2494bd4396049b0897d00f0010bb07aaea461b0037f86034e"
   else
     url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-arm64.zip"
     sha256 "65bc813566a841e500db4c000510b79649a151d541f44fcd9e173adf12e07cd6"
