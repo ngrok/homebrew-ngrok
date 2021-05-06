f1f6a9f0e18cea6ae8a2854cb5ff7f718489204a
Zach Sherman <zach@ngrok.com>
2021-05-06 09:12:44 -0700

    formulae urls link to tar not zip

--- Formula/ngrok.rb
+++ Formula/ngrok.rb
@@ -1,7 +1,7 @@
 class Ngrok < Formula
   desc 'Reverse proxy, secure introspectable tunnels to localhost'
   homepage 'https://ngrok.com'
-  url 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-arm64.zip'
+  url 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-arm64.tgz'
   sha256 '1740437cbe26bf2766b059652b829b9e5c5116c0251d486189d338f0e280c256'
   version '2.3.40'
 
