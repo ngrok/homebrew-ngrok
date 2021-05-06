9e51af04cd15d9d5f74244029f5d7e370a3ac44d
Zach Sherman <zach@ngrok.com>
2021-05-06 09:23:51 -0700

    fix arm formula sha356

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -2,11 +2,11 @@ cask "ngrok" do
   version :latest
 
   if Hardware::CPU.intel?
-    url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip",
+    url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip"
     sha256 "1740437cbe26bf2766b059652b829b9e5c5116c0251d486189d338f0e280c256",
         verified: "bin.equinox.io/"
   else
-    url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-arm64.zip",
+    url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-arm64.zip"
     sha256 "65bc813566a841e500db4c000510b79649a151d541f44fcd9e173adf12e07cd6",
         verified: "bin.equinox.io/"
   end
--- Formula/ngrok.rb
+++ Formula/ngrok.rb
@@ -2,7 +2,7 @@ class Ngrok < Formula
   desc 'Reverse proxy, secure introspectable tunnels to localhost'
   homepage 'https://ngrok.com'
   url 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-arm64.tgz'
-  sha256 '1740437cbe26bf2766b059652b829b9e5c5116c0251d486189d338f0e280c256'
+  sha256 '65bc813566a841e500db4c000510b79649a151d541f44fcd9e173adf12e07cd6'
   version '2.3.40'
 
   def install
